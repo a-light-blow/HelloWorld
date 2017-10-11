c10851cef183552103927db8403ed5cecc83efef
roy <luozuping@126.com>
2017-10-11 17:09:33 +0800

    just test

--- roy/demo/HelloWorld.java
+++ roy/demo/HelloWorld.java
@@ -2,7 +2,8 @@ package com.roy.demo;
 
 public class HelloWorld {
    public static void main(String[] args){
-       System.out.println("Hello World");
+       System.out.println("Hello World 1");
+       System.out.println("Hello World 2");
 
    }
 }
